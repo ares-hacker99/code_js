f1e99c610792e1bad9014999ec0491aeb04ba793
Jonnorich <areshacker99@gmail.com>
2025-03-01 12:31:53 +0100

    ex2 pour la gestion de formation eligne

---
+++ exercice2.js
@@ -0,0 +1,54 @@
+/*
+function calculateSum(a, b) {
+    return a + b;
+}
+console.log("La somme est:", calculateSum(5, 10));*/
+
+
+/*function calculateAverage(notes) {
+    let sum = notes.reduce((acc, note) => acc + note, 0);
+    return sum / notes.length;
+}
+let notesExemple = [15, 18, 12, 20, 14];
+console.log("La moyenne des notes est:", calculateAverage(notesExemple));*/
+
+
+/*function convertToUpperCase(chaine) {
+    return chaine.toUpperCase();
+}
+console.log("Texte en majuscules:", convertToUpperCase("j'travaille en javacript"));*/
+
+
+/*function calculateRectangleArea(longueur, largeur) {
+    return longueur * largeur;
+}
+console.log("L'aire du rectangle est:", calculateRectangleArea(3, 2));*/
+
+
+/*function isEven(nombre) {
+    return nombre % 2 === 0;
+}
+console.log("Le nombre est pair:", isEven(2));
+console.log("Le nombre est pair:", isEven(7));*/
+
+
+/*function findElement(tableau, element) {
+    return tableau.includes(element);
+}
+let mangas = ["naruto", "blue-lock", "solo leveling"];
+console.log("L'élément est présent:", findElement(mangas, "avenger"));
+console.log("L'élément est présent:", findElement(mangas, "solo leveling"));*/
+
+
+/*function fibonacci(n) {
+    if (n <= 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+console.log("Le 6ème nombre de Fibonacci est:", fibonacci("6"));*/
+
+
+/*function factorial(n) {
+    if (n === 0) return 1;
+    return n * factorial(n - 1);
+}
+console.log("La factorielle de 5 est:", factorial(2));*/
