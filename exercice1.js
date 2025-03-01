14f3a5f97255ec5b1c2f977f42b6542dbceeff02
Jonnorich <areshacker99@gmail.com>
2025-03-01 11:15:13 +0100

    code js pour gestion des formations en ligne (EXERCICE1)

---
+++ exercice1.js
@@ -0,0 +1,49 @@
+
+/*let nom = prompt("Entrez votre nom:");
+const age = prompt("Entrez votre âge:"); 
+console.log("Bienvenue, " + nom + "! Vous avez " + age + " ans.");*/
+
+
+
+/*function CCT(cout_Participant, nombre_Participants) {
+    return cout_Participant * nombre_Participants;
+}
+let total = CCT(50, 10);
+console.log("Le coût total des inscriptions est: " + total + " FCFA.");*/
+
+
+
+/*function verifierReduction(age) {
+    if (age < 18) {
+        console.log("Vous êtes éligible pour une réduction !");
+    } else {
+        console.log("Désolé, vous n'êtes pas éligible pour une réduction.");
+    }
+}
+verifierReduction(17)*/
+
+
+/*let note1 = parseFloat(prompt("Entrez la première note:"));
+let note2 = parseFloat(prompt("Entrez la deuxième note:"));
+let note3 = parseFloat(prompt("Entrez la troisième note:"));
+let moyenne = (note1 + note2 + note3) / 3;
+console.log("La moyenne des notes est: " + Math.round(moyenne * 100) / 100)*/
+
+
+/*let nom = "Jean";
+let age = 25;
+let inscriptionPayee = true;
+console.log(typeof nom);  // c'est un string
+console.log(typeof age);  // c'est un number
+console.log(typeof inscriptionPayee);  // c'est un boolean */
+
+
+/*let participants = ["Alice", "Bob", "Charlie"];
+participants.push("David");
+console.log("Liste des participants:", participants);*/
+
+
+/*let nouvelInscrit = prompt("Entrez votre nom:");
+alert("Bienvenue, " + nouvelInscrit + " !");
+console.log("Bienvenue, " + nouvelInscrit + " !");*/
+
